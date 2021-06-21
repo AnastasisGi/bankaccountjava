20ed67963c2128a430db1ea01c8a6b21a9c7a55d
AnastasisGi <anastasiosgks@gmail.com>
2021-06-21 13:02:33 +0100

    it shows the correct balance after a deposit has been made

--- src/test/java/com/anastasisgi/bankaccountjava/BankAccountTest.java
+++ src/test/java/com/anastasisgi/bankaccountjava/BankAccountTest.java
@@ -1,5 +1,6 @@
 package com.anastasisgi.bankaccountjava;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,13 @@ public class BankAccountTest {
     public void withdraw_returnsTrue_ifEnoughFunds() {
         BankAccount account = new BankAccount(500);
         assertTrue(account.withdraw(100));
+    }
+
+    @Test
+    public void deposit_an_amount() {
+        BankAccount account = new BankAccount(500);
+        account.deposit(500);
+        assertEquals(account.getBalance(), 1000);
     }
 
 }
